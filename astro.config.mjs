c50738e27d2766556ff7cf8b2528d0e1cb4b6771
Ligre <hola@ligre.studio>
2024-08-26 12:28:51 +0200

    Quit docs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,4 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-
-export default defineConfig({
-  output: 'static',
-  build: {
-    outDir: 'docs',
-  },
-});
+export default defineConfig({});
